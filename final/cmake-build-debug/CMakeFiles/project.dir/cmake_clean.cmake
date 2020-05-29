bc21cd4b5c2a3e3057edf0f36a026066bb605176
x3n0peltis <federico.stefini@studenti.unitn.it>
2020-05-29 05:48:45 -0700

    added cmake folder

---
+++ final/cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/shell.c.o"
+  "project"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
